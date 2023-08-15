9e516dd87687ee75f9253da4e753ef89abcaa2ea
thientm27 <denkhotieu1@gmail.com>
2023-08-15 15:39:37 +0700

    Clean generic repo

--- Clup-MemberShip/ClubMemberShip.Repo/IGenericRepository.cs
+++ Clup-MemberShip/ClubMemberShip.Repo/IGenericRepository.cs
@@ -7,20 +7,15 @@ namespace ClubMemberShip.Repo;
 public interface IGenericRepository<TEntity>
     where TEntity : BaseEntity
 {
-    List<TEntity> Get(params Expression<Func<TEntity, object>>[] includes);
-
     public List<TEntity> Get(
         Expression<Func<TEntity, bool>>? filter = null,
         Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
         string includeProperties = "");
 
     public void Create(TEntity entity);
-    public void CreateAutoId(TEntity entity);
     public TEntity? GetById(object? id);
     public void Update(TEntity entity);
     public void Delete(object? id);
-    public void SaveChanges();
-    Pagination<TEntity> ToPagination(int pageNumber = 0, int pageSize = 10, params Expression<Func<TEntity, object>>[] includes);
     Pagination<TEntity> ToPagination(IEnumerable<TEntity> list,int pageNumber = 0, int pageSize = 10, params Expression<Func<TEntity, object>>[] includes);
 
 }
--- Clup-MemberShip/ClubMemberShip.Repo/Repository/GenericRepo.cs
+++ Clup-MemberShip/ClubMemberShip.Repo/Repository/GenericRepo.cs
@@ -15,14 +15,6 @@ public class GenericRepo<TEntity> : IGenericRepository<TEntity>
         this.Context = context;
     }
 
-    public List<TEntity> Get(params Expression<Func<TEntity, object>>[] includes)
-    {
-        return includes
-            .Aggregate(Context.Set<TEntity>().AsQueryable(),
-                (entity, property) => entity.Include(property))
-            .Where(x => x.Status != Status.Deleted)
-            .ToList();
-    }
 
     public List<TEntity> Get(
         Expression<Func<TEntity, bool>>? filter = null,
@@ -52,31 +44,25 @@ public class GenericRepo<TEntity> : IGenericRepository<TEntity>
         return query.Where(x => x.Status != Status.Deleted).ToList();
     }
 
-    public void CreateAutoId(TEntity entity)
-    {
-        throw new NotImplementedException();
-    }
-
     public TEntity? GetById(object? id)
     {
         return Context.Set<TEntity>().Find(id);
     }
 
-    public IQueryable<TEntity> GetAll()
+    public int GetMaxId()
     {
-        // using var context = new ClubMembershipContext();
-        return Context.Set<TEntity>();
+        throw new NotImplementedException();
     }
+
 
     public void Create(TEntity entity)
     {
-        // using var context = new ClubMembershipContext();
+        entity.Status = Status.Active;
         Context.Set<TEntity>().Add(entity);
     }
 
     public void Update(TEntity entityToUpdate)
     {
-        // using var context = new ClubMembershipContext();
         Context.Set<TEntity>().Attach(entityToUpdate);
         Context.Entry(entityToUpdate).State = EntityState.Modified;
     }
@@ -89,19 +75,6 @@ public class GenericRepo<TEntity> : IGenericRepository<TEntity>
             entity.Status = Status.Deleted;
             Update(entity);
         }
-    }
-
-    public void SaveChanges()
-    {
-        Context.SaveChanges();
-    }
-
-    public Pagination<TEntity> ToPagination(int pageIndex = 0, int pageSize = 10,
-        params Expression<Func<TEntity, object>>[] includes)
-    {
-        IQueryable<TEntity> list = includes.Aggregate(Context.Set<TEntity>().AsNoTracking(),
-            (entity, property) => entity.Include(property));
-        return ToPagination(list, pageIndex, pageSize);
     }
 
     public Pagination<TEntity> ToPagination(IEnumerable<TEntity> list, int pageIndex = 0, int pageSize = 10,
