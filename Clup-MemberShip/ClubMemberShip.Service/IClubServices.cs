5436ab66c5f6ee3170273012955956dde925da6c
thientm27 <denkhotieu1@gmail.com>
2023-08-17 21:27:15 +0700

    student create group

--- Clup-MemberShip/ClubMemberShip.Service/IClubServices.cs
+++ Clup-MemberShip/ClubMemberShip.Service/IClubServices.cs
@@ -1,8 +1,13 @@
 ﻿using ClubMemberShip.Repo.Models;
+using ClubMemberShip.Service.Service;
 
 namespace ClubMemberShip.Service;
 
 public interface IClubServices : IGenericService<Club>
 {
-    
+    public Club? StudentCreateClub(Club newClub, int studentId); // also create new board and join
+    public ClubBoard? CreateClubBoard(ClubBoard newClubBoard); // also create new board and join
+    public Membership? JoinClub(Membership membership);
+    public MemberRole? JoinClubBoard(MemberRole memberRole);
+    public Result? LeaveClub(MemberRole memberRole);
 }
--- Clup-MemberShip/ClubMemberShip.Service/Service/ClubService.cs
+++ Clup-MemberShip/ClubMemberShip.Service/Service/ClubService.cs
@@ -51,9 +51,78 @@ public class ClubService : GenericService<Club>, IClubServices
         var maxId = Get().Max(o => o.Id);
         newEntity.Id = maxId + 1;
         newEntity.Status = Status.Active;
-        
+
         UnitOfWork.ClubRepo.Create(newEntity);
         UnitOfWork.SaveChange();
         return Result.Ok;
+    }
+
+    public Club? StudentCreateClub(Club newClub, int studentId)
+    {
+        // VALIDATION
+        var isExisted = UnitOfWork.ClubRepo.Get(filter: club => club.Code == newClub.Code);
+        if (isExisted.Count > 0)
+        {
+            return null;
+        }
+
+        // SET SPECIAL PROPERTIES
+        var maxId = Get().Max(o => o.Id);
+        newClub.Id = maxId + 1;
+        newClub.Status = Status.Pending; // WAIT FOR ADMIN ACCEPT
+
+        // CREATE CLUB
+        UnitOfWork.ClubRepo.Create(newClub);
+        // CREATE CLUB BOARD
+        var newClubBoardCreated = CreateClubBoard(new ClubBoard
+        {
+            ClubId = newClub.Id,
+            Name = "Owner",
+            LongDecription = "Manage the club",
+            ShortDecription = "Owner club",
+        });
+        // CREATE STUDENT JOIN CLUB (membership)
+        var memberShip = JoinClub(new Membership
+        {
+            ClubId = newClub.Id,
+            JoinDate = DateTime.Today,
+            QuitDate = null,
+            NickName = "Club Creator",
+            StudentId = studentId,
+        });
+
+        // CREATE STUDENT JOIN CLUB BOARD (owner)
+        JoinClubBoard(new MemberRole
+        {
+            MembershipId = memberShip.Id,
+            ClubBoardId = 0,
+            StartDay = DateTime.Today,
+            EndDay = default,
+            Role = 1,
+        });
+        
+        
+        UnitOfWork.SaveChange();
+        return newClub;
+    }
+
+    public ClubBoard? CreateClubBoard(ClubBoard newClubBoard)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Membership? JoinClub(Membership membership)
+    {
+        throw new NotImplementedException();
+    }
+
+    public MemberRole? JoinClubBoard(MemberRole memberRole)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Result? LeaveClub(MemberRole memberRole)
+    {
+        throw new NotImplementedException();
     }
 }
