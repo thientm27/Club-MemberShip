8cfd702ddc8cdb501d3ee62e6f359c1b3fa56385
thientm27 <denkhotieu1@gmail.com>
2023-08-21 08:43:05 +0700

    Setup club view page

---
+++ Clup-MemberShip/ClubMemberShip.Present/Pages/PageUser/ClubDetail.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ClubMemberShip.Repo.Models;
+
+namespace ClubMemberShip.Web.Pages.PageUser
+{
+    public class ClubDetailModel : PageModel
+    {
+        private readonly ClubMemberShip.Repo.Models.ClubMembershipContext _context;
+
+        public ClubDetailModel(ClubMemberShip.Repo.Models.ClubMembershipContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Club> Club { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Clubs != null)
+            {
+                Club = await _context.Clubs.ToListAsync();
+            }
+        }
+    }
+}
