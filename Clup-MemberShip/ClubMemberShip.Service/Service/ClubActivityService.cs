d72a1399ccb18ee56e92eea43d29893fd079c086
thientm27 <denkhotieu1@gmail.com>
2023-08-22 21:56:49 +0700

    Add Grade,Major for GetListStudentInActivity

--- Clup-MemberShip/ClubMemberShip.Service/Service/ClubActivityService.cs
+++ Clup-MemberShip/ClubMemberShip.Service/Service/ClubActivityService.cs
@@ -53,7 +53,7 @@ public class ClubActivityService : GenericService<ClubActivity>, IClubActivitySe
 
     public List<Student>? GetListStudentInActivity(int id)
     {
-        var listJoin = UnitOfWork.ParticipantRepo.Get(filter: o => o.ClubActivityId == id);
+        var listJoin = UnitOfWork.ParticipantRepo.Get(filter: o => o.ClubActivityId == id, includeProperties: "Grade,Major");
 
         var listMemberShipId = listJoin.Select(o => o.MembershipId);
         var listMemberShip = UnitOfWork.MemberShipRepo.Get(filter: o => listMemberShipId.Contains(o.Id));
